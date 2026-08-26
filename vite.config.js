c4d8679b2d4ff1ab063279864793e9222e35d177
vmhexacorp <venkatesan.muthukrishnan@hexacorp.com>
2026-08-26 12:23:45 +0530

    vite deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: './TechLife',
   build: {
     outDir: 'dist',
     sourcemap: false,
