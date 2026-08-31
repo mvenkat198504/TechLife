f449d7eaca2d0c9ed5a356d3f33bbdcdf93f7fb7
vmhexacorp <venkatesan.muthukrishnan@hexacorp.com>
2026-08-31 10:00:33 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TechLife/'
+  //base: '/TechLife/'
+  base: process.env.VITE_BASE_PATH || '/TechLife',
 
 });
